21e250570a0dbe3a1f3bcbd96f09555aa4d55882
danny30312 <66548062+danny30312@users.noreply.github.com>
2021-04-16 15:06:22 -0400

    Add files via upload

--- mesi.py
+++ mesi.py
@@ -23,6 +23,7 @@ if __name__ == "__main__":
         line_size   = inst_dict["line_size"]
         mem_size    = inst_dict["mem_size"]
         cache_way   = inst_dict["cache_way"]
+        run_node    = inst_dict["run_node"]
         node_inst   = {}
         for i in range(num_cache):
             node_inst[i] = inst_dict[f"node_{i}"]
@@ -45,40 +46,58 @@ if __name__ == "__main__":
     if cache_type == "d":
         for i in range(num_cache):
             if cache_dict != None:
-                cache_list.append(cache.direct_cache(i, cache_size, line_size, mem_size, cache_dict[str(i)]["cache"], cache_dict[str(i)]["LSR"]))
+                cache_list.append(cache.direct_cache(i, cache_size, line_size, mem_size, cache_dict[str(i)]["cache"], cache_dict[str(i)]["LSR"], cache_dict[str(i)]["PC"]))
             else:
-                cache_list.append(cache.direct_cache(i, cache_size, line_size, mem_size, None, []))
+                cache_list.append(cache.direct_cache(i, cache_size, line_size, mem_size, None, [], 0))
     elif cache_type == "f":
         for i in range(num_cache):
             if cache_dict != None:
-                cache_list.append(cache.fully_cache(i, cache_size, line_size, mem_size, cache_dict[str(i)]["cache"], cache_dict[str(i)]["LSR"]))
+                cache_list.append(cache.fully_cache(i, cache_size, line_size, mem_size, cache_dict[str(i)]["cache"], cache_dict[str(i)]["LSR"], cache_dict[str(i)]["PC"]))
             else:
-                cache_list.append(cache.fully_cache(i, cache_size, line_size, mem_size, None, []))
+                cache_list.append(cache.fully_cache(i, cache_size, line_size, mem_size, None, [], 0))
     else:
         for i in range(num_cache):
             if cache_dict != None:
-                cache_list.append(cache.nway_cache(i, cache_size, line_size, mem_size, cache_way, cache_dict[str(i)]["cache"], cache_dict[str(i)]["LSR"]))
+                cache_list.append(cache.nway_cache(i, cache_size, line_size, mem_size, cache_way, cache_dict[str(i)]["cache"], cache_dict[str(i)]["LSR"], cache_dict[str(i)]["PC"]))
             else:
-                cache_list.append(cache.nway_cache(i, cache_size, line_size, mem_size, cache_way, None, []))
+                cache_list.append(cache.nway_cache(i, cache_size, line_size, mem_size, cache_way, None, [], 0))
 
-    for node, inst_list in node_inst.items():
-        for k in inst_list:
-            # Bus operation
-            bus_reply = []
-            bus_info = cache_list[node].cache_operation(k[0], k[1])
-            print(bus_info["bus_info"])
-            for i in cache_list:
-                if i.cache_ID != node:
-                    bus_reply.append(i.bus_operation(bus_info["bus_info"], k[1])["bus_reply"])
-                    print(bus_reply)
 
-            # Change from E to S
-            if bus_info["bus_info"] == "BusRd" and True in bus_reply:
-                cache_list[node].cache_dict[bus_info["dict_key"]]["protocol"] = "S"
+    k = node_inst[run_node][cache_list[run_node].PC]
+    # Bus operation
+    bus_reply = []
+    bus_info = cache_list[run_node].cache_operation(k[0], k[1])
+    #print(bus_info["bus_info"])
+    for i in cache_list:
+        if i.cache_ID != run_node:
+            bus_reply.append(i.bus_operation(bus_info["bus_info"], k[1])["bus_reply"])
+    #print(bus_reply)
+
+    # Change from E to S
+    if bus_info["bus_info"] == "BusRd" and True in bus_reply:
+        cache_list[run_node].cache_dict[bus_info["dict_key"]]["protocol"] = "S"
+    
+    # PC + 1
+    cache_list[run_node].PC = cache_list[run_node].PC + 1
+
+    # for node, inst_list in node_inst.items():
+    #     for k in inst_list:
+    #         # Bus operation
+    #         bus_reply = []
+    #         bus_info = cache_list[node].cache_operation(k[0], k[1])
+    #         print(bus_info["bus_info"])
+    #         for i in cache_list:
+    #             if i.cache_ID != node:
+    #                 bus_reply.append(i.bus_operation(bus_info["bus_info"], k[1])["bus_reply"])
+    #                 print(bus_reply)
+
+    #         # Change from E to S
+    #         if bus_info["bus_info"] == "BusRd" and True in bus_reply:
+    #             cache_list[node].cache_dict[bus_info["dict_key"]]["protocol"] = "S"
 
     # Print out cache for debug
-    for i in cache_list:
-        i.print_cache()
+    # for i in cache_list:
+    #     i.print_cache()
 
     # Dump ot cache json file
     dump_dict = {}
